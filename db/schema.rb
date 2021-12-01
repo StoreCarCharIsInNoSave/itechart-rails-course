49b40a588e97c7748871bd7a1a7c3e798438f6d6
unknown <anonvtrede@gmail.com>
2021-12-01 15:18:36 +0300

    Removed login from user

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_01_001447) do
+ActiveRecord::Schema.define(version: 2021_12_01_121028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "login", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.datetime "remember_created_at"
