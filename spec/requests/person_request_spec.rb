c59832528bf5a7140ec5e3f702739367de8bf4a0
unknown <anonvtrede@gmail.com>
2021-12-10 15:34:17 +0300

    created some specs for person controller

--- spec/requests/person_request_spec.rb
+++ spec/requests/person_request_spec.rb
@@ -24,16 +24,31 @@ RSpec.describe "People", type: :request do
     expect(response.body).to include('Create person')
   end
 
-
-  # it "should not get all person of user without user" do
-  #   sign_out @user
-  #   get person_index_path
-  #   expect(response.body).to include('You must be signed in to do that')
-  # end
-
+  it "should not get new person with existing user" do
+    sign_out @user
+    get new_person_path
+    expect(response).to have_http_status(302)
+  end
 
 
+  it "should not get all person of user without user" do
+    sign_out @user
+    get person_index_path
+    expect(response).to have_http_status(302)
+  end
 
 
+  it 'should check impossibility of deletion last person' do
+    get person_index_path
+    expect(response.body).to include('You can\'t delete the last person')
+  end
+
+  it 'should check possibility of deletion not last person' do
+    p = Person.new(title: 'sometitle', name:'fsdfsdfsd',lastname:'fsfsdfsd')
+    p.user = @user
+    p.save
+    get person_index_path
+    expect(response.body).not_to include('You can\'t delete the last person')
+  end
 
 end
