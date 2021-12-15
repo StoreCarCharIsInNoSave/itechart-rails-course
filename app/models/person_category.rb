fac3e3fc670138560cadb1102a10c63d52dcc8ad
unknown <anonvtrede@gmail.com>
2021-12-16 01:09:52 +0300

    created sign about relationship on edit/new transaction forms

--- app/models/person_category.rb
+++ app/models/person_category.rb
@@ -4,4 +4,18 @@ class PersonCategory < ApplicationRecord
   belongs_to :person
   belongs_to :category
   has_many :money_transactions
+
+
+  def is_debit_to_string
+    category.debit ? 'Debit' : 'Credit'
+  end
+
+  def select_title
+    "[Person]: #{person.title}, [Category]: #{category.title}, [Operation type]: #{self.is_debit_to_string}"
+  end
+
+
+
 end
+
+
