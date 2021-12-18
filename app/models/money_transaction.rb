4d1a975bb86902e568157d363b0cb83ccfc3030a
unknown <anonvtrede@gmail.com>
2021-12-18 18:49:43 +0300

    Added validation to moneyTransaction model and rspecs for it

--- app/models/money_transaction.rb
+++ app/models/money_transaction.rb
@@ -4,7 +4,7 @@ class MoneyTransaction < ApplicationRecord
   belongs_to :person_category
   belongs_to :note, optional: true
   before_destroy :destroy_note
-
+  validates :amount_value, presence: true, numericality: { greater_than: 0 }
   def destroy_note
     note = Note.find_by(id: note_id)
     note.destroy if note.present?
