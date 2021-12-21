45b73c1a9778ba203aafea4ae31841cce28ed8a4
unknown <anonvtrede@gmail.com>
2021-12-21 23:29:07 +0300

    created update action for note model

--- app/controllers/note_controller.rb
+++ app/controllers/note_controller.rb
@@ -11,4 +11,29 @@ class NoteController < ApplicationController
     flash[:notice] = 'Note successfully deleted.'
     redirect_to category_info_path(category)
   end
+
+  def new
+    @note = Note.new
+  end
+
+  def edit
+    @note = Note.find(params[:id])
+  end
+
+  def update
+    @note = Note.find(params[:id])
+    if @note.update_attributes(note_params)
+      flash[:notice] = 'Note successfully updated.'
+      redirect_to category_info_path(MoneyTransaction.find_by_note_id(@note.id).person_category.category)
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:body, :color)
+  end
+
 end
