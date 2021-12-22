ea6bc55354d296f5a598d0651ff98790004f54f2
unknown <anonvtrede@gmail.com>
2021-12-22 14:56:27 +0300

    DRY for note controller

--- app/controllers/note_controller.rb
+++ app/controllers/note_controller.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 class NoteController < ApplicationController
-  def show
-    @note = Note.find(params[:id])
-  end
+  before_action :require_signed_user, only: %i[new create edit update destroy]
+  before_action :require_same_signed_user, only: %i[edit update destroy]
+  before_action :find_note, only: %i[show edit update destroy]
+  def show; end
 
   def destroy
-    @note = Note.find(params[:id])
     category = MoneyTransaction.find_by_note_id(@note.id).person_category.category
     @note.destroy
     flash[:notice] = 'Note successfully deleted.'
@@ -17,12 +17,9 @@ class NoteController < ApplicationController
     @note = Note.new
   end
 
-  def edit
-    @note = Note.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @note = Note.find(params[:id])
     if @note.update_attributes(note_params)
       flash[:notice] = 'Note successfully updated.'
       redirect_to category_info_path(MoneyTransaction.find_by_note_id(@note.id).person_category.category)
@@ -35,5 +32,24 @@ class NoteController < ApplicationController
 
   def note_params
     params.require(:note).permit(:body, :color)
+  end
+
+  def find_note
+    @note = Note.find(params[:id])
+  end
+
+  def require_signed_user
+    return if user_signed_in?
+
+    flash[:alert] = 'You must be signed in to do that'
+    redirect_to root_path
+  end
+
+  def require_same_signed_user
+    transaction = MoneyTransaction.find_by_note_id(params[:id])
+    return if current_user == transaction.person_category.person.user
+
+    flash[:alert] = 'You must be the owner to do this.'
+    redirect_to root_path
   end
 end
