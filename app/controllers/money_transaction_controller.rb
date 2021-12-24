b8d20aad59946b40bdfa3a4b12ac19c6c1d38bb2
unknown <anonvtrede@gmail.com>
2021-12-24 16:19:23 +0300

    added require signed user validation to important action in money transaction model

--- app/controllers/money_transaction_controller.rb
+++ app/controllers/money_transaction_controller.rb
@@ -2,7 +2,7 @@
 
 class MoneyTransactionController < ApplicationController
   before_action :money_transaction_find, only: %i[edit update destroy]
-  before_action :require_signed_user, only: %i[new create edit update destroy index]
+  before_action :require_signed_user, only: %i[new create edit update destroy index important]
   before_action :require_same_signed_user, only: %i[edit update destroy]
 
   def index
