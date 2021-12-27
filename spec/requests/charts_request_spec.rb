5950e97e92569bca48dd5d19e3b847530c55220d
unknown <anonvtrede@gmail.com>
2021-12-27 17:11:15 +0300

    created spec for chart controller

--- spec/requests/charts_request_spec.rb
+++ spec/requests/charts_request_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "Charts", type: :request do
-
+  let(:user) { User.create(email: 'someemail@gmail.com', password: 'somepassword') }
+  before do
+    sign_in user
+  end
+  it 'should get access to chart page' do
+    get information_page_path
+    expect(response.body).to include('Chart information')
+  end
+  it 'should get access to chart page without user' do
+    sign_out user
+    get information_page_path
+    expect(response.body).to include('example')
+  end
 end
